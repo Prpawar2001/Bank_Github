d9d3ee9270b0dd6898f67cf91caaa9193be48417
Prpawar2001 <prpawar2001@gmail.com>
2022-10-02 10:46:14 +0530

    Committing bank features view balance, deposit, withdraw, transfer.

--- Bank_Github/src/com/Idfc/entity/User.java
+++ Bank_Github/src/com/Idfc/entity/User.java
@@ -1,11 +1,14 @@
 package com.Idfc.entity;
 
 public class User {
+	
+	//creating private variable to store user data
 	private int accNo = 1001;
 	private String passwd = "Pawar2001";
 	private String name = "Prathamesh pawar";
 	private int accBal = 40000;
 	
+//	Creating getters and setters for accessing above data.
 	public int getAccNo() {
 		return accNo;
 	}
